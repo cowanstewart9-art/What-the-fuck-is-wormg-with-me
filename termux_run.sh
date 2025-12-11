0abcd0fe2f7909d6ed4761c9aa82a37207e2ae43
cowanstewart9-art <cowanstewart9@gmail.com>
2025-12-10 22:25:23 -0800

    Update termux_run.sh

--- termux_run.sh
+++ termux_run.sh
@@ -4,7 +4,7 @@
 if ! command -v python3 &> /dev/null; then
     echo "Python 3 is not installed."
     echo "Please install it by running: pkg install python"
-    exit 1
+    
 fi
 
 echo "Starting Tic Tac Toe for Termux..."
